e487f9c188e9db6b50235c101877c7baac40265c
WNDH-VD3m <77713690+WNDH-VD3m@users.noreply.github.com>
2021-02-01 17:53:44 +0800

    function_two_parameter

---
+++ demo8.php
@@ -0,0 +1,16 @@
+<?php
+
+function peo($name,$age)
+{
+    echo "My name is " . $name . " and my age is " . $age;
+}
+
+peo("Xiaoming", 18);
+echo PHP_EOL;
+peo("Xiaohong", 21);
+echo PHP_EOL;
+peo("Xiaogang", 39);
+echo PHP_EOL;
+
+
+?>
