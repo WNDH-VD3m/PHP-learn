b799a3cbd2ae45e9295b49d51766b425a5d0f419
WNDH-VD3m <77713690+WNDH-VD3m@users.noreply.github.com>
2021-02-01 17:08:43 +0800

    递增/递减运算符
    ++ x 预递增 x 加 1，然后返回 x
    
    x ++ 后递增 返回 x，然后 x 加 1

---
+++ demo4.php
@@ -0,0 +1,25 @@
+<?php
+
+
+$x=10;
+echo ++$x; // 输出11
+echo PHP_EOL;
+
+$y=10;
+echo $y++; // 输出10
+echo PHP_EOL;
+echo $y;
+
+echo PHP_EOL;
+
+$z=5;
+echo --$z; // 输出4
+echo PHP_EOL;
+
+$i=5;
+echo $i--; // 输出5
+echo PHP_EOL;
+echo $i;
+
+
+?>
