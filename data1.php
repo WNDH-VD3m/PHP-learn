3179c2ed557b98caa825cf58fa20dff9fa8a3122
WNDH-VD3m <77713690+WNDH-VD3m@users.noreply.github.com>
2021-02-24 16:07:26 +0800

    php-practice

---
+++ data1.php
@@ -0,0 +1,12 @@
+<?php
+
+echo date("Y/m/d") . "<br>";
+
+echo date("m/d") . "<br>";
+
+echo date("Y.m.d") . "<br>";
+
+echo date("Y-m-d");
+
+
+?>
---
+++ mul_array.php
@@ -0,0 +1,27 @@
+<pre>
+<?php
+
+    $sites = array
+    (
+        "Ao"=>array
+        (
+            "A1",
+            "A1.com"
+        ),
+        "Bo"=>array
+        (
+            "B1",
+            "B1.com"
+        ),
+        "Co"=>array
+        (
+            "C1",
+            "C1.com"
+        )
+    );
+
+    print_r($sites);
+
+?>
+
+</pre>
